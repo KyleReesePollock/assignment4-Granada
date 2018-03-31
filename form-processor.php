9476589b2701d362aa09966a9113509cdecfe9a2
jhirs10 <jhirs10@u.rochester.edu>
2018-03-31 19:45:59 -0400

    change to database host name

--- form-processor.php
+++ form-processor.php
@@ -1,6 +1,6 @@
 <?php
 	// step 1: create a database connection
-	$dbhost = '66.147.242.186';
+	$dbhost = 'localhost';
 	$dbuser = "urcscon3_granad";
 	$dbpass = "coffee1N";
 	$dbname = "urcscon3_granad";
